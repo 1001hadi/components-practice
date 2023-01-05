d0a9975aec7f82d9c1906c6fb9b43ceb3542472b
1001hadi <hadi.jaf.1001@gmail.com>
2023-01-05 09:49:10 -0700

    btn componenet compeleted and tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from "react";
-import "./App.css";
+import {
+  GoBell,
+  GoCloudDownload,
+  GoDatabase,
+  GoBeaker,
+  GoZap,
+} from "react-icons/go";
+import Button from "./components/Button";
 
 function App() {
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
   return (
     <div className="App">
-      <h1>Components App</h1>
+      <div>
+        <Button success rounded outline className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Click Here!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoBeaker />
+          Click There!
+        </Button>
+      </div>
+      <div>
+        <Button primary>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button outline rounded warning>
+          <GoDatabase />
+          GO to Home
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoZap />
+          Last Deal!
+        </Button>
+      </div>
     </div>
   );
 }
